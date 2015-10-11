e2ac4e40c1d016c7c1e85046b0defa5038ce3b28
LiuYang <liuyang070424@163.com>
2015-10-11 20:13:44 +0800

    some try to reduce the runtime 注释中的部分添加了sumRest来尝试减少运行时间

--- 40_Combination_Sum_II_NonRecursive.cpp
+++ 40_Combination_Sum_II_NonRecursive.cpp
@@ -3,8 +3,14 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
         sort(candidates.begin(),candidates.end(),less_equal<int>());//从小到大排序
+        /*vector<int> sumRest;//做一个存放sum的数组，来判断i有没有必要继续往下做，比如[1 3 4]和10的情景
+         int sum=0;
+         for(vector<int>::reverse_iterator it=candidates.rbegin();it!=candidates.rend();it++){
+         sum+=(*it);
+         sumRest.insert(sumRest.begin(),sum);
+         }*/
         vector<int> res;
-        int i=0,j=0,rest=target;//i是当前处理数字的position，j遍历到candidates.end()的时候遍历结束
+        int i=0,rest=target;//i是当前处理数字的position，j遍历到candidates.end()的时候遍历结束
         stack<int> posStack;
         //while(j<candidates.size()){
         while(1){
@@ -21,6 +27,8 @@ public:
                 rest-=candidates[i];
                 res.push_back(candidates[i]);
                 i++;
+                //if(sumRest[i]<rest)
+                //    break;
                 if(i>=candidates.size()){//从j开始后所有的数据加起来也不够target的情况，则要先出栈一个数据再跳出循环后出栈
                     int pos=posStack.top();
                     posStack.pop();
