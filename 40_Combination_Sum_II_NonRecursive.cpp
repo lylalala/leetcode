385affac5ee967e5c37d33f9f6d51f4f3f96930f
LiuYang <liuyang070424@163.com>
2015-10-11 19:34:07 +0800

    some improvement: remove redundancy 去掉一些冗余的代码

--- 40_Combination_Sum_II_NonRecursive.cpp
+++ 40_Combination_Sum_II_NonRecursive.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
-        sort(candidates.begin(),candidates.end(),less_equal<int>());
+        sort(candidates.begin(),candidates.end(),less_equal<int>());//从小到大排序
         vector<int> res;
-        int i=0,j=0,rest=target;
+        int i=0,j=0,rest=target;//i是当前处理数字的position，j遍历到candidates.end()的时候遍历结束
         stack<int> posStack;
-        while(j<candidates.size()){
+        //while(j<candidates.size()){
+        while(1){
             while(1){
-                if(candidates[i]>rest){
+                if(candidates[i]>rest){//当前数据大于剩余的数据，则跳出压栈，进行出栈
                     break;
                 }else if(candidates[i]==rest){
                     res.push_back(candidates[i]);
@@ -20,7 +21,7 @@ public:
                 rest-=candidates[i];
                 res.push_back(candidates[i]);
                 i++;
-                if(i>=candidates.size()){
+                if(i>=candidates.size()){//从j开始后所有的数据加起来也不够target的情况，则要先出栈一个数据再跳出循环后出栈
                     int pos=posStack.top();
                     posStack.pop();
                     res.pop_back();
@@ -37,17 +38,10 @@ public:
                     i++;
                 posStack.pop();
                 res.pop_back();
-            }else{
-                i++;
-                while(candidates[i]==candidates[i-1]){
-                    i++;
-                    j++;
-                }
-                if(i>=candidates.size())
-                    return result;
-            }
-            if(posStack.empty())
-                j++;
+            }else//当res里面没有数字并且当前数字较大（或者等于）的时候，直接结束这个程序
+                return result;
+            /*if(posStack.empty())
+             j++;*/
         }
         return result;
     }
